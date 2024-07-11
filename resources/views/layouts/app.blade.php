48e63659646ac7364eb507d52a5a6106c85f795f
Minister <mundestephane13@gmail.com>
2024-07-11 23:48:25 +0100

    Added tailwind config to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,17 @@
 <title>@yield('title')</title>
 
 <script src="{{ asset('js/tailwind.js') }}"></script>
+<script>
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: 'deepskyblue'
+            }
+        }
+    }
+}
+</script>
 </head>
 
 <body class="antialiased font-sans bg-gray-200">
