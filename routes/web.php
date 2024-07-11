4ca1baa1d3570cd5b85f4d49951165e925b31afc
Minister <mundestephane13@gmail.com>
2024-07-12 00:21:22 +0100

    Added the about page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return view('home');});
+Route::get('/', function () { return view('home'); });
+Route::get('/apropos', function () {
+    return view('about');
+});
 
 Route::get('/?', function () {
     return view('welcome');
