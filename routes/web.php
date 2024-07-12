3f43d1f22f7bd01363cf1f46d0b2305029e5b7b9
Minister <mundestephane13@gmail.com>
2024-07-12 04:24:47 +0100

    Added more routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('home'); })->name('home');
@@ -11,4 +12,35 @@ Route::view('contacte', 'contact')->name('contact');
 
 Route::get('/?', function () {
     return view('welcome');
+});
+
+Route::view('register', 'register');
+Route::post('register', function (Request $request) {
+    $attributes = $request->validate();
+});
+
+
+Route::view('login', 'login')->name('login');
+Route::post('login', function () {
+    $attributes = collect(
+        request()->validate(
+            [
+                'email' => ['required', 'email'],
+                'password' => ['required', 'string'],
+                'remember' => ['sometimes', 'boolean']
+            ]
+        )
+    );
+
+    if (! auth()->attempt($attributes->except('remember')->all(), $attributes->get('remember'))) {
+        throw \Illuminate\Validation\ValidationException::withMessages(
+            [
+                'login' => 'Échèc de connexion. veillez consulter votre adresse e-mail et mot de passe.'
+            ]
+        );
+    }
+
+    request()->session()->regenerate();
+
+    return redirect()->intended('/');
 });
