d1bde984638231616e87726b7143fc3f68b0d4ca
Minister <mundestephane13@gmail.com>
2024-07-12 01:51:53 +0100

    Added name to the home route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('home'); });
+Route::get('/', function () { return view('home'); })->name('home');
 Route::get('/apropos', function () {
     return view('about');
 })->name('about');
