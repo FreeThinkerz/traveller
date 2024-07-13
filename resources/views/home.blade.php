fce12c7c74b80ea0fdacc36e6061dba848050a29
Minister <mundestephane13@gmail.com>
2024-07-13 11:36:38 +0100

    translated the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <?php
-$inNav = 'passengers';
+$inNav = 'passagers';
 $sliders = [
     'avenir-voyages.jpeg',
     'buca-voyages.jpeg',
@@ -24,7 +24,7 @@ $destinations = [
         'thumbnail' => 'https://via.placeholder.com/150x100?text=Buea',
     ],
     [
-        'name' => 'Yaounde',
+        'name' => 'Yaoundé',
         'price' => 120000,
         'thumbnail' => 'https://via.placeholder.com/150x100?text=Yaounde',
     ],
@@ -96,7 +96,7 @@ $destination_list= [
 ?>
 @extends('layouts.app')
 
-@section('title', 'B. Trans-Reserve')
+@section('title', 'B. Trans-Réserve')
 
 @section('content')
     <div class="">
@@ -110,21 +110,21 @@ $destination_list= [
                 </div>
             </div>
             <div class="px-5 desktop:px-40 tv:px-60 laptop:px-32">
-                {{-- 2nd section --}}
+                {{-- 2ème section --}}
                 <div class="md:-mt-24 -mt-16 flex flex-col">
                     <div class="flex  w-full">
                         <button
-                            class="flex items-center space-x-2 p-4 @if ($inNav === 'passengers') bg-primary text-white @else bg-white text-black @endif ">
+                            class="flex items-center space-x-2 p-4 @if ($inNav === 'passagers') bg-primary text-white @else bg-white text-black @endif ">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
                                 <path fill="#4f46e5"
                                     d="M10.5 9A3.5 3.5 0 1 1 14 5.5A3.504 3.504 0 0 1 10.5 9m0-5A1.5 1.5 0 1 0 12 5.5A1.5 1.5 0 0 0 10.5 4m11.974 27.313L19.34 24h-7.101a4.01 4.01 0 0 1-3.867-2.97l-1.634-6.127a3.899 3.899 0 0 1 7.535-2.009L15.1 16H21v2h-7.436l-1.223-4.59a1.9 1.9 0 0 0-3.67.978l1.633 6.126A2.005 2.005 0 0 0 12.239 22h8.42l3.654 8.525zM30 6h-4V2h-2v4h-4v2h4v4h2V8h4z" />
                                 <path fill="#4f46e5"
                                     d="M18 28H7.768a2 2 0 0 1-1.933-1.485L2.033 12.258l1.933-.516L7.768 26H18Z" />
                             </svg>
-                            <p class="text-lg font-semibold">Passengers</p>
+                            <p class="text-lg font-semibold">Passagers</p>
                         </button>
                         <button
-                            class="flex items-center space-x-2 p-4 @if ($inNav !== 'passengers') bg-primary text-white @else bg-white text-black @endif ">
+                            class="flex items-center space-x-2 p-4 @if ($inNav !== 'passagers') bg-primary text-white @else bg-white text-black @endif ">
                             <svg xmlns="http://www.w3.org/2000/svg" class="size-8" viewBox="0 0 48 48">
                                 <path fill="#111" stroke="#111" stroke-linecap="round" stroke-linejoin="round"
                                     d="M8.339 23.276v10.767l15.654 2.941V13.519" />
@@ -133,16 +133,16 @@ $destination_list= [
                                 <path fill="none" stroke="#111" stroke-linecap="round" stroke-linejoin="round"
                                     d="m28.775 22.075l13.386 1.844a1.176 1.176 0 0 0 1.236-1.643l-3.835-8.632l-15.57-2.628L8.34 13.77l-3.73 8.016a1.176 1.176 0 0 0 1.149 1.67l13.338-.927a1.47 1.47 0 0 0 1.24-.865l3.655-8.144l3.653 7.727a1.47 1.47 0 0 0 1.129.828m-17.87 8.589l2.941.313" />
                             </svg>
-                            <p class="text-lg font-semibold">Passels</p>
+                            <p class="text-lg font-semibold">Colis</p>
                         </button>
                         <div class="w-full bg-trasparent"></div>
                     </div>
-                    {{-- conditional secton 1 --}}
-                    @if ($inNav === 'passengers')
+                    {{-- section conditionnelle 1 --}}
+                    @if ($inNav === 'passagers')
                         <div class="bg-gray-50 p-4 shadow-md space-y-3 rounded-b-md">
                             <div class="flex space-x-2 items-center">
-                                <span class="rounded-3xl bg-primary p-2 text-white text-sm shadow">first text</span>
-                                <span class="rounded-3xl p-2 text-sm shadow">second text</span>
+                                <span class="rounded-3xl bg-primary p-2 text-white text-sm shadow">premier texte</span>
+                                <span class="rounded-3xl p-2 text-sm shadow">deuxième texte</span>
                             </div>
                             <div class="flex justify-center md:justify-start items-center gap-2 flex-wrap">
                                 <div class="flex shadow space-x-2 items-center p-2  rounded-md flex-grow max-w-md">
@@ -156,7 +156,7 @@ $destination_list= [
                                         </svg>
                                     </span>
                                     <div class="flex flex-col">
-                                        <h4 class="font-bold text-sm">Departure</h4>
+                                        <h4 class="font-bold text-sm">Départ</h4>
                                         <p class="text-muted text-sm">Lorem, ipsum.</p>
                                     </div>
                                 </div>
@@ -179,14 +179,14 @@ $destination_list= [
                                         </svg>
                                     </span>
                                     <div class="flex flex-col">
-                                        <h4 class="font-bold text-sm">Arrival</h4>
+                                        <h4 class="font-bold text-sm">Arrivée</h4>
                                         <p class="text-muted text-sm">Lorem, ipsum.</p>
                                     </div>
                                 </div>
                                 <div class="flex shadow space-x-2 items-center p-2  rounded-md flex-grow max-w-md">
-                                    <span>icon</span>
+                                    <span>icône</span>
                                     <div class="flex flex-col">
-                                        <h4 class="font-bold text-sm">Hello from You</h4>
+                                        <h4 class="font-bold text-sm">Bonjour de votre part</h4>
                                         <p class="text-muted text-sm">Lorem, ipsum.</p>
                                     </div>
                                 </div>
@@ -199,7 +199,7 @@ $destination_list= [
                                         </svg>
                                     </span>
                                     <div class="flex flex-col">
-                                        <h4 class="font-bold text-sm">Passengers</h4>
+                                        <h4 class="font-bold text-sm">Passagers</h4>
                                         <p class="text-muted text-sm">20</p>
                                     </div>
                                 </div>
@@ -209,30 +209,27 @@ $destination_list= [
                                         <path fill="#fff"
                                             d="M15.58 16.8L12 14.5l-3.58 2.3l1.08-4.12L6.21 10l4.25-.26L12 5.8l1.54 3.94l4.25.26l-3.29 2.68M20 12a2 2 0 0 1 2-2V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2a2 2 0 0 1-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 1-2-2" />
                                     </svg>
-                                    <span>BOOK NOW</span>
+                                    <span>RÉSERVER MAINTENANT</span>
                                 </button>
                             </div>
                         </div>
                     @else
                         {{-- section 2 --}}
                         <div>
-                            second section
+                            deuxième section
                         </div>
                     @endif
                 </div>
 
-                {{-- 3rd section advantages --}}
+                {{-- 3ème section avantages --}}
                 <div class="mt-14">
-                    <h2 class="font-bold text-xl"> Our Advantages</h2>
+                    <h2 class="font-bold text-xl"> Nos Avantages</h2>
                     <div class="grid gap-10 tablet:grid-cols-2 laptop:grid-cols-3 laptop:grid mt-10">
                         <div class="flex justify-center items-center space-x-2 max-w-xs">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">Book Flights Fast</h3>
-                                <p class="text-muted max-w-xs">Discover incredible fares on flights & hotels from a wide
-                                    range
-                                    of
-                                    providers.
+                                <h3 class="font-bold">Réservez des vols rapidement</h3>
+                                <p class="text-muted max-w-xs">Découvrez des tarifs incroyables sur les vols et les hôtels auprès d'un large éventail de fournisseurs.
                                 </p>
                             </div>
 
@@ -241,9 +238,8 @@ $destination_list= [
                         <div class="flex justify-center items-center space-x-2 max-w-md">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">24/7 Access and Information at Your Fingertips</h3>
-                                <p class="text-muted max-w-xs">Book and manage your trips anytime,
-                                    anywhere - all from your device.
+                                <h3 class="font-bold">Accès 24/7 et informations à portée de main</h3>
+                                <p class="text-muted max-w-xs">Réservez et gérez vos voyages à tout moment, n'importe où - tout depuis votre appareil.
                                 </p>
                             </div>
 
@@ -252,27 +248,24 @@ $destination_list= [
                         <div class="flex justify-center items-center space-x-2 max-w-xs">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">Travel Anytime, Anywhere</h3>
-                                <p class="text-muted max-w-xs">Manage trips 24/7 with a few taps on your device.
+                                <h3 class="font-bold">Voyagez n'importe quand, n'importe où</h3>
+                                <p class="text-muted max-w-xs">Gérez vos voyages 24/7 en quelques clics sur votre appareil.
                                 </p>
                             </div>
                         </div>
                         <div class="flex justify-center items-center space-x-2 max-w-xs">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">Stress-Free Travel Planning</h3>
-                                <p class="text-muted max-w-xs">Effortlessly search,
-                                    compare, and book your dream trip.
+                                <h3 class="font-bold">Planification de voyage sans stress</h3>
+                                <p class="text-muted max-w-xs">Recherchez, comparez et réservez facilement le voyage de vos rêves.
                                 </p>
                             </div>
                         </div>
                         <div class="flex justify-center items-center space-x-2 max-w-xs">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">Peace of Mind Booking</h3>
-                                <p class="text-muted max-w-xs">Secure your trip with trusted payment options and clear
-                                    travel
-                                    policies.
+                                <h3 class="font-bold">Réservation en toute tranquillité</h3>
+                                <p class="text-muted max-w-xs">Sécurisez votre voyage avec des options de paiement fiables et des politiques de voyage claires.
 
                                 </p>
                             </div>
@@ -281,8 +274,8 @@ $destination_list= [
                         <div class="flex justify-center items-center space-x-2 max-w-xs">
                             <x-icons.time_saving class="size-24 self-start" />
                             <div class="flex flex-col gap-2">
-                                <h3 class="font-bold">Top Deals, Wide Choice</h3>
-                                <p class="text-muted max-w-xs">Discover great fares on flights & hotels from many providers
+                                <h3 class="font-bold">Meilleures offres, large choix</h3>
+                                <p class="text-muted max-w-xs">Découvrez d'excellents tarifs sur les vols et les hôtels auprès de nombreux fournisseurs
 
                                 </p>
                             </div>
@@ -314,7 +307,7 @@ $destination_list= [
 
         {{-- section 2 --}}
         <div class="bg-white px-5 desktop:px-60 laptop:px-32 py-10 mt-10 flex flex-col">
-            <h1 class="font-bold text-xl text-center text-primary">Popular Destinations with Departure directly from Douala
+            <h1 class="font-bold text-xl text-center text-primary">Destinations populaires avec départ direct de Douala
             </h1>
             <div class="laptop:grid-cols-3 tablet:grid-cols-2  grid gap-10 mt-10">
 
@@ -322,51 +315,48 @@ $destination_list= [
                     <x-destination-card :is-default="$loop->first" :destination="$destination" />
                 @endforeach
             </div>
-            <button class="p-4 bg-white shadow rounded mt-10 mx-auto "> Show all Destinations</button>
+            <button class="p-4 bg-white shadow rounded mt-10 mx-auto "> Afficher toutes les destinations</button>
         </div>
 
-        {{-- section 3 Our Services and Destination --}}
+        {{-- section 3 Nos services et destinations --}}
 
         <div class="bg-primary/95 px-5 desktop:px-60 laptop:px-32 py-10">
-            {{-- our services --}}
-            <div>
+            {{-- nos services --}}
                 <div class="flex flex-col justify-center items-center gap-10">
-                    <h1 class="font-bold text-xl text-white">Our Services</h1>
+                    <h1 class="font-bold text-xl text-white">Nos Services</h1>
                     <div class="flex justify-center items-center gap-5">
                         <div class="flex flex-col justify-center items-center">
                             <p class="text-secondary font-bold text-lg">10K+</p>
-                            <p class="text-white">Happy Customers</p>
+                            <p class="text-white">Clients Satisfaits</p>
                         </div>
                         <div class="flex flex-col justify-center items-center">
                             <p class="text-secondary font-bold text-lg">100K+</p>
-                            <p class="text-white">Dialy Bookings</p>
+                            <p class="text-white">Réservations Quotidiennes</p>
                         </div>
                         <div class="flex flex-col justify-center items-center">
                             <p class="text-secondary font-bold text-lg">10K+</p>
-                            <p class="text-white">Parcel deliveries</p>
+                            <p class="text-white">Livraisons de Colis</p>
                         </div>
                     </div>
                 </div>
                 <div class="flex flex-col-reverse tablet:grid tablet:grid-cols-2   gap-10 text-white mt-10">
                     <div class="flex flex-col">
-                        <h2 class="text-2xl font-bold">Transporter le gen</h2>
-                        <p class="text-sm text-muted mt-5">we offer save and secure transportation of people , delivery of
-                            packages to all parts of Cameroon and
-                            pride ourselfs on our service's reliability efficiency and determination to meet your standards
+                        <h2 class="text-2xl font-bold">Transporter les gens</h2>
+                        <p class="text-sm text-muted mt-5">Nous offrons un transport sûr et sécurisé des personnes, la livraison de colis dans toutes les régions du Cameroun et nous sommes fiers de la fiabilité, de l'efficacité et de la détermination de notre service à répondre à vos attentes.
                         </p>
                         <ul class="flex mt-5">
                             <div class="flex flex-col">
-                                <li class="">Multiple Payment Gateways</li>
-                                <li class="">Seamless Search and Filtering</li>
-                                <li class="">Interactive Seat Selection</li>
+                                <li class="">Multiples Passerelles de Paiement</li>
+                                <li class="">Recherche et Filtrage Fluides</li>
+                                <li class="">Sélection de Siège Interactive</li>
                             </div>
                             <div class="flex flex-col">
-                                <li class="">Interactive Seat Selection</li>
-                                <li class="">Real-time Availability and Pricing</li>
-                                <li class="">Additional Features</li>
+                                <li class="">Sélection de Siège Interactive</li>
+                                <li class="">Disponibilité et Prix en Temps Réel</li>
+                                <li class="">Fonctionnalités Supplémentaires</li>
                             </div>
                         </ul>
-                        <button class="p-3 bg-white text-black w-fit rounded px-5 mt-5">Check it Out</button>
+                        <button class="p-3 bg-white text-black w-fit rounded px-5 mt-5">Découvrir</button>
                     </div>
                     <div>
                         <img class="object-fit object-center size-full" src="{{ asset('/images/bus.png') }}"
@@ -375,9 +365,9 @@ $destination_list= [
 
                 </div>
             </div>
-            {{-- Destination List --}}
+            {{-- Liste des destinations --}}
             <div class="mt-10">
-                <h1 class="font-bold text-lg text-center text-white">Distination List</h1>
+                <h1 class="font-bold text-lg text-center text-white">Liste des Destinations</h1>
                 <div class="grid tablet:grid-cols-2 laptop:grid-cols-3 gap-10 mt-10">
                     @foreach ($destination_list as $destination)
                         <x-destination-list :destination="$destination" />
